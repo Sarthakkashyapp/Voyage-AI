9bea0568f3c1d6d834a5d7eacdfa69a046c528ec
sarthakkashyap <sarthakkashyap2621@gmail.com>
2024-08-10 19:54:47 +0530

    Header update

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -51,11 +51,11 @@ function Header() {
   };
 
   return (
-    <div className='p-3 shadow-sm flex justify-between items-center px-5 bg-gray-200 gap-16'>
-      <img src='/Orange.png' className='h-[40px] w-[160px] object-cover rounded-md'/>
+    <div className='p-3 shadow-sm flex justify-between items-center px-5 bg-gray-200 gap-4'>
+      <img src='/Orange.png' className='h-[40px] w-[140px] object-cover rounded-md'/>
       <div >
         { user? (// Conditional rendering based on user state
-          <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2'>
           <a href='/create-trip'>
             <Button variant="outline"
               className="rounded-full">+ Create Trip</Button>
