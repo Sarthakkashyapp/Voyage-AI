95ec0c45ca6b925302492da0e501e72689a8fc4d
sarthakkashyap <sarthakkashyap2621@gmail.com>
2024-12-27 19:48:04 +0530

    Updated tailwind.config.js to use ESM syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -77,4 +77,4 @@ module.exports = {
     },
   },
   // kuch tha yahaan 
-}
+};
